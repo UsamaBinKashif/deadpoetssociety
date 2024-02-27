efaaf4fc647a1ac298e705833f9af920657970ac
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-02-28 00:24:46 +0500

    server side

--- server-side/controllers/user.controllers.js
+++ server-side/controllers/user.controllers.js
@@ -1,13 +1,44 @@
 const asyncHandler = require("express-async-handler");
 
-// @desc    Auth user & get token
-// @route   POST /api/users/auth
+// @desc    signin user & get token
+// @route   POST /api/users/signin
 // @access  Public
-
-const auth_user = asyncHandler(async (req, res) => {
+const signin = asyncHandler(async (req, res) => {
   res.send("AUTH");
 });
 
+// @desc    Register a new user
+// @route   POST/api/users/sigup
+// @access  Public
+const signup = asyncHandler(async (req, res) => {
+  res.send("REGISTER");
+});
+
+// @desc    Signout user / clear cookie
+// @route   POST /api/users/Signout
+// @access  Public
+const signout = asyncHandler(async (req, res) => {
+  res.send("REGISTER");
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserDetails = asyncHandler(async (req, res) => {
+  res.send("REGISTER");
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUser = asyncHandler(async (req, res) => {
+  res.send("REGISTER");
+});
+
 module.exports = {
-  auth_user,
+  signin,
+  signup,
+  signout,
+  getUserDetails,
+  updateUser,
 };
--- server-side/routes/user.routes.js
+++ server-side/routes/user.routes.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { auth_user } = require("../controllers/user.controllers");
+const {
+
+  signup,
+  signin,
+  signout,
+  getUserDetails,
+  updateUser,
+} = require("../controllers/user.controllers");
 // importing router
 const router = express.Router();
 
-router.post("/signup", auth_user);
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/signout", signout);
+router.route("/profile").get(getUserDetails).put(updateUser);
 
 module.exports = router;

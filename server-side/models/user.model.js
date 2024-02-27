fdf93253e67fa95839783c39797348e37fea94c4
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-02-28 00:30:53 +0500

    server side

---
+++ server-side/models/user.model.js
@@ -0,0 +1,32 @@
+// This file defines the Mongoose schema and model for the 'user' collection.
+
+const mongoose = require("mongoose");
+
+// Defining the schema for the user collection
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // Adding timestamps to track creation and modification dates
+    timestamps: true,
+  }
+);
+
+// Creating the Mongoose model for the 'user' collection
+const USER = mongoose.model("user", userSchema);
+
+
+module.exports = USER;
--- server-side/routes/user.routes.js
+++ server-side/routes/user.routes.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const {
-
   signup,
   signin,
   signout,
   getUserDetails,
   updateUser,
 } = require("../controllers/user.controllers");
-// importing router
+
+// Creating an instance of Express Router
 const router = express.Router();
 
+// Route for user signup
 router.post("/signup", signup);
+
+// Route for user signin
 router.post("/signin", signin);
+
+// Route for user signout
 router.post("/signout", signout);
+
+// Route for getting and updating user profile
 router.route("/profile").get(getUserDetails).put(updateUser);
 
 module.exports = router;

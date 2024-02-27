528a9a2f522a553284785ad78b51856762da0a1d
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-02-28 00:42:06 +0500

    server side

--- server-side/controllers/user.controllers.js
+++ server-side/controllers/user.controllers.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const USER = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
 // @desc    signin user & get token
 // @route   POST /api/users/signin
@@ -11,7 +13,22 @@ const signin = asyncHandler(async (req, res) => {
 // @route   POST/api/users/sigup
 // @access  Public
 const signup = asyncHandler(async (req, res) => {
-  res.send("REGISTER");
+  const { name, email, password } = req.body;
+
+  const userExists = USER.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email Address already exists!");
+  }
+  const hash = await bcrypt.hash(password, 12);
+  const user = await USER.create({ name, email, password: hash });
+
+  if (user) {
+    res.status(201).json({ _id: user._id, email: user.email, name: user.name, });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data!");
+  }
 });
 
 // @desc    Signout user / clear cookie

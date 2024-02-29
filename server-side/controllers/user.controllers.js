d34ebaa40ae216b4f18300cb881e16a211163949
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-03-01 02:29:35 +0500

    update user route

--- server-side/controllers/user.controllers.js
+++ server-side/controllers/user.controllers.js
@@ -22,17 +22,15 @@ const signin = asyncHandler(async (req, res) => {
   } else {
     generateToken(res, user._id);
 
-    res
-      .status(201)
-      .json({
-        message: "Signed in",
-        user: {
-          id: user._id,
-          email: user.email,
-          name: user.name,
-          profile_image: user.profile_image,
-        },
-      });
+    res.status(201).json({
+      message: "Signed in",
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+        profile_image: user.profile_image,
+      },
+    });
   }
 });
 
@@ -88,7 +86,32 @@ const getUserDetails = asyncHandler(async (req, res) => {
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("REGISTER");
+  const user = await USER.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    if (req.body.name) {
+      const userImage = `https://source.boringavatars.com/beam/100/${req.body.name}?colors=FF8900,00000,00000,FF8900,000000`;
+      user.profile_image = userImage;
+    }
+    if (req.body.password) {
+      const hash = await bcrypt.hash(req.body.password, 12);
+      user.password = hash;
+    }
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      message: "Updated user profile!",
+      user: {
+        id: updatedUser._id,
+        email: updatedUser.email,
+        name: updatedUser.name,
+        profile_image: updatedUser.profile_image,
+      },
+    });
+  } else {
+    res.status(400);
+    throw new Error("User not found!");
+  }
 });
 
 module.exports = {

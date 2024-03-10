1c1712d48faf721bd733dfd729fd55a84f5bfed9
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-03-10 16:27:57 +0500

    assign files

--- client-side/pages/index.jsx
+++ client-side/pages/index.jsx
@@ -1,30 +1,20 @@
 // Import the action creators 'incremented' and 'decremented' from the 'counterSlice'.
 import { decremented, incremented } from "@/src/store/features/counterSlice";
+import Signin from "@/src/utils/forms/Signin";
 
 // Import the 'Head' component from 'next/head' to set document head metadata.
 import Head from "next/head";
 
-// Import 'useDispatch' and 'useSelector' hooks from 'react-redux' to interact with the Redux store.
-import { useDispatch, useSelector } from "react-redux";
 
-// Define your main application component.
 export default function Home() {
-  // Use 'useSelector' to select the 'counter' state from the Redux store.
-  const { value } = useSelector((state) => state.counter);
-
-  // Use 'useDispatch' to get access to the Redux store's 'dispatch' function.
-  const dispatch = useDispatch();
-
   return (
     <>
       {/* Set document head metadata */}
       <Head>
-        <title>Boilerplate</title>
+        <title>DEAD POETS SOCIETY | LOGIN</title>
       </Head>
 
-      <main>
-       <h1>USAMA</h1>
-      </main>
+      <Signin />
     </>
   );
 }

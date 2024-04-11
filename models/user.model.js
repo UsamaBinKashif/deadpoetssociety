dfcca87c13bc727026237712f1956a57c5750618
Usama Ahmed <usamabinkashif.98@gmail.com>
2024-04-11 15:05:46 +0500

    Added files from API

---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+// This file defines the Mongoose schema and model for the 'user' collection.
+
+const mongoose = require("mongoose");
+
+// Defining the schema for the user collection
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+    profile_image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // Adding timestamps to track creation and modification dates
+    timestamps: true,
+  }
+);
+
+// Creating the Mongoose model for the 'user' collection
+const USER = mongoose.model("user", userSchema);
+
+module.exports = USER;
